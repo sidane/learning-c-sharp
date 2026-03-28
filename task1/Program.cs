e433fc368599ff86221de1b0a37a8c244937d2ea
Niall Mullally <niall.mullally@gmail.com>
2026-03-28 10:31:37 +0000

    Further updates to BankAccount

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,33 +1,55 @@
-﻿BankAccount bankAccount = new BankAccount("Joe Bloggs", 100.00m);
-bankAccount.Deposit(50.00m);
-bankAccount.Withdraw(10.00m);
-Console.WriteLine($"Your current balance is {bankAccount.CurrentBalance()}");
+﻿BankAccount bankAccount1 = new BankAccount("Joe Bloggs", 100.00m);
+bankAccount1.Deposit(50.00m);
+try
+{
+  bankAccount1.Withdraw(10.00m);
+}
+catch (InsufficientFundsException ex)
+{
+  Console.WriteLine(ex.Message);
+}
+Console.WriteLine($"{bankAccount1.Name} your current balance is {bankAccount1.Balance}");
+
+BankAccount bankAccount2 = new BankAccount("Peter Parker", 10.00m);
+bankAccount2.Deposit(5.00m);
+try
+{
+  bankAccount2.Withdraw(20.00m);
+}
+catch (InsufficientFundsException ex)
+{
+  Console.WriteLine(ex.Message);
+}
+Console.WriteLine($"{bankAccount2.Name} your current balance is {bankAccount2.Balance}");
 
 public class BankAccount
 {
-  public string name;
-  public decimal balance;
+  public string Name { get; private set; }
+  public decimal Balance { get; private set; }
 
-  public BankAccount(string name, decimal initialBalance)
+  public BankAccount(string accountHolderName, decimal initialBalance)
   {
-     this.name = name;
-     balance = initialBalance;
+     Name = accountHolderName;
+     Balance = initialBalance;
   }
 
   public void Deposit(decimal amount)
   {
-    balance += amount;
+    Balance += amount;
   }
 
   public void Withdraw(decimal amount)
   {
-    balance -= amount;
-  }
+    if (Balance < amount) {
+      throw new InsufficientFundsException(amount);
+    }
 
-  public decimal CurrentBalance()
-  {
-    return balance;
+    Balance -= amount;
   }
 }
 
-
+public class InsufficientFundsException : Exception
+{
+  public InsufficientFundsException(decimal amount)
+    : base($"Insufficient funds: attempted to withdraw {amount}") {}
+}
