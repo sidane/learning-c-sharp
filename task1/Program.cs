369685ef2f3942ac294a22c8b3318e0aaf7e6f73
Niall Mullally <niall.mullally@gmail.com>
2026-03-28 08:42:31 +0000

    Initial commit
    Creating basic BankAccount class

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿BankAccount bankAccount = new BankAccount("Joe Bloggs", 100.00m);
+bankAccount.Deposit(50.00m);
+bankAccount.Withdraw(10.00m);
+Console.WriteLine($"Your current balance is {bankAccount.CurrentBalance()}");
+
+public class BankAccount
+{
+  public string name;
+  public decimal balance;
+
+  public BankAccount(string name, decimal initialBalance)
+  {
+     this.name = name;
+     balance = initialBalance;
+  }
+
+  public void Deposit(decimal amount)
+  {
+    balance += amount;
+  }
+
+  public void Withdraw(decimal amount)
+  {
+    balance -= amount;
+  }
+
+  public decimal CurrentBalance()
+  {
+    return balance;
+  }
+}
+
+
